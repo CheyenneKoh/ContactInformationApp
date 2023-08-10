0c235e097b9888f4b71913615ab8e26eeb696afa
CheyenneKoh <cheyennekoh@hotmail.com>
2023-08-10 18:19:09 +0800

    refactor(hooks): declare constant for url

--- src/hooks/useContacts.ts
+++ src/hooks/useContacts.ts
@@ -2,11 +2,11 @@ import {Contact} from 'models/Contact';
 import {useQuery} from 'react-query';
 import {QUERY_KEYS} from '../services/QueryClientService';
 
+const URL = 'https://mocki.io/v1/75ca9c88-8da0-4513-a588-5c99ebd9a0b3';
+
 function getContacts() {
   return new Promise<Contact[]>(async resolve => {
-    const response = await fetch(
-      'https://mocki.io/v1/75ca9c88-8da0-4513-a588-5c99ebd9a0b3',
-    );
+    const response = await fetch(URL);
 
     if (!response.ok) {
       throw new Error('Network response was not ok');
